2c2f54994f247a50694625dd052ea9cd22ecc791
Ukaypromise <promiseukaimo@gmail.com>
2023-06-15 21:32:05 +0100

    Add header and subtitle

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book, or rent a car faster - quickly and easily!
+        </h1>
+        <p className="hero__subtitle">
+            streamlines the process of finding, booking, and renting a car.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
