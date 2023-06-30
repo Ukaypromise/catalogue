c083f79c83459c450ff5a73ed7b11762250dcabd
Ukaypromise <promiseukaimo@gmail.com>
2023-06-30 19:57:13 +0100

    fix image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,7 +25,7 @@ const Hero = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-            <Image src='' alt="image"/>
+            <Image src='/hero.png' alt="image" fill className="object-contain"/>
         </div>
       </div>
     </div>
