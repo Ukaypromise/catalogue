0bf22655cf1dbb1701f9630ee14285bd9a357343
Ukaypromise <promiseukaimo@gmail.com>
2023-07-01 22:30:47 +0100

    ******

--- components/index.ts
+++ components/index.ts
@@ -2,7 +2,7 @@ import CarCard from "./CarCard";
 import CustomButton from "./CustomButton";
 import CustomFilter from "./CustomFilter";
 import Footer from "./Footer";
-import NavBar from "./Navbar";
+import Navbar from "./Navbar";
 import ShowMore from "./ShowMore";
 import SearchBar from "./SearchBar";
 import Hero from "./Hero";
@@ -13,7 +13,7 @@ export {
   CustomButton,
   CustomFilter,
   Footer,
-  NavBar,
+  Navbar,
   ShowMore,
   SearchBar,
 };
